9adc9e97a9d96ea8d962824149b0d4d4dc98d7ef
grapefruit13 <liam.heo11@gmail.com>
2024-04-14 15:46:53 +0900

    chore: types, utils, constants, hooks 디렉토리 src 루트에 추가

---
+++ src/constants/auth.ts
@@ -0,0 +1,12 @@
+/**
+ * 25분
+ * SS * MM
+ */
+export const ACCESS_TOKEN_EXPIRED_TIME = 60 * 25;
+/**
+ * 13일
+ * SS * MM * HH * DD
+ */
+export const REFRESH_TOKEN_EXPIRED_TIME = 60 * 60 * 24 * 13;
+
+export const AXIOS_TIMEOUT = 5000;
---
+++ src/constants/inputNames.ts
@@ -0,0 +1,8 @@
+export const INPUT_NAMES = {
+  profileImageUrl: 'profileImageUrl',
+  email: 'email',
+  nickname: 'nickname',
+  password: 'newPassword',
+  passwordConfirm: 'passwordConfirm',
+  image: 'image',
+};
