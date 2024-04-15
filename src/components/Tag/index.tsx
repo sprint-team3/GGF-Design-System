7d4a58456b08fdbab1d88107d73d8b4bcaba94de
designsoo <kheesoo10@gmail.com>
2024-04-15 15:40:19 +0900

    refactor: 모집타입 prop을 태그의 variant로 변경, variant 타입 재설정, children prop 추가

--- src/components/Tag/index.tsx
+++ src/components/Tag/index.tsx
@@ -1,19 +1,17 @@
 import classNames from 'classnames/bind';
 
-import { formatPostTypeToKR } from '@/utils';
-
-import { PostTypesEN } from '@/types';
-
 import styles from './Tag.module.scss';
+import { ReactNode } from 'react';
 
 const cx = classNames.bind(styles);
 
 type TagProps = {
-  postType: PostTypesEN | string;
+  children: ReactNode;
+  variant?: 'default' | 'primary' | 'secondary' | 'tertiary';
 };
 
-const Tag = ({ postType }: TagProps) => {
-  return <div className={cx(`tag-${postType}`)}>{formatPostTypeToKR(postType)}</div>;
+const Tag = ({ children, variant = 'default' }: TagProps) => {
+  return <div className={cx(`tag-${variant}`)}>{children}</div>;
 };
 
 export default Tag;
