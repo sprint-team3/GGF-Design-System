35a6828f5ac56bd502af4852e1cd2e25eaef0b5b
grapefruit13 <liam.heo11@gmail.com>
2024-04-15 14:43:18 +0900

    refactor: FormDropdown props 순서 정렬, Image태그 대신 img 사용

--- src/components/inputs/FormDropdown.tsx
+++ src/components/inputs/FormDropdown.tsx
@@ -1,11 +1,9 @@
-import Image from 'next/image';
-
 import { useEffect, useState } from 'react';
 
 import classNames from 'classnames/bind';
 import { useFormContext } from 'react-hook-form';
 
-import { SVGS } from '@/constants';
+import { SVGS } from '@/constants/index';
 
 import useTogglePopup from '@/hooks/useTogglePopup';
 
@@ -84,11 +82,7 @@ export const FormDropdown = ({
             disabled={isDisabled}
             onClick={togglePopup}
           >
-            {isOpen ? (
-              <Image src={activeUrl} alt={activeAlt} width={24} height={24} />
-            ) : (
-              <Image src={defaultUrl} alt={defaultAlt} width={24} height={24} />
-            )}
+            <img className={cx('img')} src={isOpen ? activeUrl : defaultUrl} alt={isOpen ? activeAlt : defaultAlt} />
           </button>
         </div>
 
