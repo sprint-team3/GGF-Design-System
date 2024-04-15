68d92700f3618aa74eef02343f3d6ee059d4c0a6
designsoo <kheesoo10@gmail.com>
2024-04-15 15:56:58 +0900

    refactor: 커스텀된 modal 컴포넌트 삭제 및 공용 컴포넌트 타입 재설정

--- src/components/modals/CommonModal.tsx
+++ src/components/modals/CommonModal.tsx
@@ -1,23 +1,17 @@
-import Image from 'next/image';
-
-import { ReactNode, MouseEventHandler } from 'react';
+import { ReactNode } from 'react';
 
 import classNames from 'classnames/bind';
 import ReactModal from 'react-modal';
-
-import { SVGS } from '@/constants';
 
 import styles from './CommonModal.module.scss';
 
 const cx = classNames.bind(styles);
 
-const { url, alt } = SVGS.arrow.chevron;
-
 type CommonModalProps = {
   openModal: boolean;
-  onClose: MouseEventHandler<HTMLButtonElement>;
   title: string;
   renderContent: ReactNode;
+  onClose: () => void;
 };
 
 export const CommonModal = ({ openModal, onClose, title, renderContent }: CommonModalProps) => {
@@ -35,13 +29,6 @@ export const CommonModal = ({ openModal, onClose, title, renderContent }: Common
     >
       <div className={cx('modal-inner')}>
         <header className={cx('modal-header')}>
-          <div className={cx('lg-hidden')}>
-            <nav className={cx('modal-mobile-nav')}>
-              <button onClick={onClose}>
-                <Image src={url} alt={alt} width={48} height={48} />
-              </button>
-            </nav>
-          </div>
           <h2 className={cx('modal-header-title')}>{title}</h2>
         </header>
         <div className={cx('modal-content')}>{renderContent}</div>
--- src/components/modals/index.ts
+++ src/components/modals/index.ts
@@ -1,4 +1,3 @@
 export * from './CommonModal';
-export * from './CustomCommonModal';
 export * from './ConfirmModal';
 export * from './ModalButton';
