9813e64ae899d4bbdf4cb524572bb7cecfad3fd0
ccwnc <ccwnc337@gmail.com>
2024-04-15 13:14:07 +0900

    refactor: EmptyAlarm 컴포넌트 import 오류 수정

--- src/components/header/EmptyAlarm/index.tsx
+++ src/components/header/EmptyAlarm/index.tsx
@@ -1,8 +1,6 @@
-import Image from 'next/image';
-
 import classNames from 'classnames/bind';
 
-import { SVGS } from '@/constants';
+import { SVGS } from '@/constants/images';
 
 import styles from './EmptyAlarm.module.scss';
 
@@ -13,7 +11,7 @@ const { url, alt } = SVGS.alarm.off;
 const EmptyAlarm = () => {
   return (
     <div className={cx('empty-alarm')}>
-      <Image src={url} alt={alt} width={24} height={24} />
+      <img src={url} alt={alt} width={24} height={24} />
       <span className={cx('text')}>알림이 없습니다</span>
     </div>
   );
