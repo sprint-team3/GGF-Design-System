a5aee0e70af3bb46fcf09011668866ae29f135fc
designsoo <kheesoo10@gmail.com>
2024-04-16 13:14:26 +0900

    chore: types 폴더의 index.ts 파일을 외부 모듈로 선언

--- types/declaration.d.ts
+++ types/declaration.d.ts
@@ -2,6 +2,10 @@ declare module '@/constants' {
   export * from '@/constants/index';
 }
 
+declare module '@/types' {
+  export * from '@/types/index';
+}
+
 declare module '@/utils' {
   export * from '@/utils/index';
 }
