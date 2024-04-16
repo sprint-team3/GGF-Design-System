d45e9d5eef8b15823d9b7a92fa0d607a6d020d37
CheeseB <2489ckckck@naver.com>
2024-04-16 16:46:47 +0900

    feat: Button/Filter/Tab 컴포넌트 스토리북 추가

---
+++ src/stories/CountButton.stories.ts
@@ -0,0 +1,18 @@
+import { CountButton } from '@/components/buttons';
+
+import type { Meta, StoryObj } from '@storybook/react';
+
+const meta = {
+  title: 'Buttons/CountButton',
+  component: CountButton,
+} satisfies Meta<typeof CountButton>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Example: Story = {
+  args: {
+    label: '참여 인원',
+    isDisabled: false,
+  },
+};
