64266022abbdcf5c8cd7de52a7ec600d15909ea9
ccwnc <ccwnc337@gmail.com>
2024-04-16 17:50:57 +0900

    refactor: Pagination 컴포넌트 스토리북 수정

--- src/stories/pagination.stories.ts
+++ src/stories/pagination.stories.ts
@@ -3,7 +3,7 @@ import Pagination from '@/components/Pagination';
 import type { Meta, StoryObj } from '@storybook/react';
 
 const meta = {
-  title: 'Pagination',
+  title: 'Pagination/Pagination',
   component: Pagination,
 } satisfies Meta<typeof Pagination>;
 
@@ -12,10 +12,11 @@ type Story = StoryObj<typeof meta>;
 
 export const Example: Story = {
   args: {
-    totalCount: 100,
+    totalCount: 1000,
     pageState: 0,
     postPerPage: 10,
     defaultPageSize: 1000,
+    pageLimit: 5,
     onClick: () => {},
   },
 };
