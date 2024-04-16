96c0f96b087853eb2836d2a12caf6d984ff22687
designsoo <kheesoo10@gmail.com>
2024-04-16 13:04:31 +0900

    chore: SCSS config 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { resolve } from 'path';
+
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, `./src`),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@use "@/styles/main.scss" as *;`,
+      },
+    },
+  },
+});
