9d96d88a13cbc97223e7c4b8838dff85d9b14008
designsoo <kheesoo10@gmail.com>
2024-04-16 15:43:09 +0900

    feat: Common Modal 컴포넌트 스토리북 추가

---
+++ src/stories/CommonModal.stories.ts
@@ -0,0 +1,20 @@
+import { CommonModal } from '@/components/modals';
+
+import type { Meta, StoryObj } from '@storybook/react';
+
+const meta = {
+  title: 'Modals/CommonModal',
+  component: CommonModal,
+} satisfies Meta<typeof CommonModal>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Example: Story = {
+  args: {
+    openModal: true,
+    title: 'CommonModal',
+    renderContent: 'Content',
+    onClose: () => {},
+  },
+};
