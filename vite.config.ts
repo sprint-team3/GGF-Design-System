99c32039cea489a48592c05be34359b493ac42f4
designsoo <kheesoo10@gmail.com>
2024-04-18 16:13:14 +0900

    chore: vite.config 절대경로 수정

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': resolve(__dirname, `./src`),
+      '@': resolve(__dirname, `./src/lib`),
     },
   },
   css: {
