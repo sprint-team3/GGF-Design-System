68bd7afb53d94b7b6d74c61407295ae5b0c97484
CheeseB <2489ckckck@naver.com>
2024-04-19 15:42:06 +0900

    build: vite 빌드 세팅

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { resolve } from 'path';
+import * as path from 'path';
 
-import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [dts()],
   resolve: {
     alias: {
       '@': resolve(__dirname, `./src/lib`),
@@ -16,6 +17,24 @@ export default defineConfig({
       scss: {
         additionalData: `@use "@/styles/main.scss" as *;`,
       },
+    },
+  },
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/lib/index.ts'),
+      name: 'index',
+      fileName: 'index',
+    },
+    rollupOptions: {
+      external: ['react'],
+      output: {
+        globals: {
+          react: 'React',
+        },
+      },
+    },
+    commonjsOptions: {
+      esmExternals: ['react'],
     },
   },
 });
