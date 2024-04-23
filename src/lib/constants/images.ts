48b3b2333cb9ad700fe5b04d401323e4788bb6a2
CheeseB <2489ckckck@naver.com>
2024-04-23 17:39:24 +0900

    fix: 이미지를 CDN에서 가져오도록 수정

--- src/lib/constants/images.ts
+++ src/lib/constants/images.ts
@@ -1,209 +1,211 @@
+const CLOUD_IMAGE_URL = 'https://d2vcremxatlmjn.cloudfront.net/images/icons';
+
 export const SVGS = {
   profile: {
-    url: '/assets/ic-profile.svg',
+    url: `${CLOUD_IMAGE_URL}/ic-profile.svg`,
     alt: 'default profile image',
   },
 
   star: {
     empty: {
-      url: '/assets/ic-star-empty.svg',
+      url: `${CLOUD_IMAGE_URL}/ic-star-empty.svg`,
       alt: 'empty star icon',
     },
     filled: {
-      url: '/assets/ic-star-filled.svg',
+      url: `${CLOUD_IMAGE_URL}/ic-star-filled.svg`,
       alt: 'filled star icon',
     },
     error: {
-      url: '/assets/ic-star-error.svg',
+      url: `${CLOUD_IMAGE_URL}/ic-star-error.svg`,
       alt: 'error star icon',
     },
   },
 
   arrow: {
     bottom: {
-      url: '/assets/ic-arrow-bottom.svg',
+      url: `${CLOUD_IMAGE_URL}/ic-arrow-bottom.svg`,
       alt: 'down arrow',
     },
     top: {
-      url: '/assets/ic-arrow-top.svg',
+      url: `${CLOUD_IMAGE_URL}/ic-arrow-top.svg`,
       alt: 'up arrow',
     },
     left: {
       default: {
-        url: '/assets/ic-arrow-left-default.svg',
+        url: `${CLOUD_IMAGE_URL}/ic-arrow-left-default.svg`,
         alt: 'left arrow',
       },
       active: {
-        url: '/assets/ic-arrow-left-active.svg',
+        url: `${CLOUD_IMAGE_URL}/ic-arrow-left-active.svg`,
         alt: 'left arrow',
       },
     },
     right: {
       default: {
-        url: '/assets/ic-arrow-right-default.svg',
+        url: `${CLOUD_IMAGE_URL}/ic-arrow-right-default.svg`,
         alt: 'right arrow',
       },
       active: {
-        url: '/assets/ic-arrow-right-active.svg',
+        url: `${CLOUD_IMAGE_URL}/ic-arrow-right-active.svg`,
         alt: 'right arrow',
       },
     },
     down: {
       default: {
-        url: '/assets/ic-arrow-down-default.svg',
+        url: `${CLOUD_IMAGE_URL}/ic-arrow-down-default.svg`,
         alt: 'dropdown arrow default',
       },
       active: {
-        url: '/assets/ic-arrow-down-active.svg',
+        url: `${CLOUD_IMAGE_URL}/ic-arrow-down-active.svg`,
         alt: 'dropdown arrow active',
       },
     },
     chevron: {
-      url: '/assets/ic-chevron.svg',
+      url: `${CLOUD_IMAGE_URL}/ic-chevron.svg`,
       alt: 'back button',
     },
   },
   alarm: {
     empty: {
-      url: '/assets/ic-alarm-empty.svg',
+      url: `${CLOUD_IMAGE_URL}/ic-alarm-empty.svg`,
       alt: 'empty alarm',
     },
     full: {
-      url: '/assets/ic-alarm-full.svg',
+      url: `${CLOUD_IMAGE_URL}/ic-alarm-full.svg`,
       alt: 'filled alarm',
     },
     off: {
-      url: '/assets/ic-alarm-off.svg',
+      url: `${CLOUD_IMAGE_URL}/ic-alarm-off.svg`,
       alt: 'off alarm',
     },
   },
 
   eye: {
     on: {
-      url: '/assets/ic-eye-on.svg',
+      url: `${CLOUD_IMAGE_URL}/ic-eye-on.svg`,
       alt: 'show password',
     },
     off: {
-      url: '/assets/ic-eye-off.svg',
+      url: `${CLOUD_IMAGE_URL}/ic-eye-off.svg`,
       alt: 'hide password',
     },
   },
 
   drawerMenu: {
-    url: '/assets/ic-menu.svg',
+    url: `${CLOUD_IMAGE_URL}/ic-menu.svg`,
     alt: 'mobile menu',
   },
 
   button: {
     add: {
       default: {
-        url: '/assets/ic-add.svg',
+        url: `${CLOUD_IMAGE_URL}/ic-add.svg`,
         alt: 'add button',
       },
       active: {
-        url: '/assets/ic-add-active.svg',
+        url: `${CLOUD_IMAGE_URL}/ic-add-active.svg`,
         alt: 'add button',
       },
     },
     remove: {
       default: {
-        url: '/assets/ic-remove.svg',
+        url: `${CLOUD_IMAGE_URL}/ic-remove.svg`,
         alt: 'remove button',
       },
       active: {
-        url: '/assets/ic-remove-active.svg',
+        url: `${CLOUD_IMAGE_URL}/ic-remove-active.svg`,
         alt: 'remove button',
       },
     },
 
     more: {
-      url: '/assets/ic-more.svg',
+      url: `${CLOUD_IMAGE_URL}/ic-more.svg`,
       alt: 'menu button',
     },
     setting: {
-      url: '/assets/ic-setting.svg',
+      url: `${CLOUD_IMAGE_URL}/ic-setting.svg`,
       alt: 'setting button',
     },
   },
 
   calendar: {
     default: {
-      url: '/assets/ic-calendar-default.svg',
+      url: `${CLOUD_IMAGE_URL}/ic-calendar-default.svg`,
       alt: 'calendar icon',
     },
     active: {
-      url: '/assets/ic-calendar-active.svg',
+      url: `${CLOUD_IMAGE_URL}/ic-calendar-active.svg`,
       alt: 'calendar icon',
     },
   },
 
   location: {
     default: {
-      url: '/assets/ic-location-default.svg',
+      url: `${CLOUD_IMAGE_URL}/ic-location-default.svg`,
       alt: 'location icon',
     },
     active: {
-      url: '/assets/ic-location-active.svg',
+      url: `${CLOUD_IMAGE_URL}/ic-location-active.svg`,
       alt: 'location icon',
     },
   },
 
   search: {
-    url: '/assets/ic-search.svg',
+    url: `${CLOUD_IMAGE_URL}/ic-search.svg`,
     alt: 'magnifier icon',
   },
 
   chatbot: {
     default: {
-      url: '/assets/ic-chatbot.svg',
+      url: `${CLOUD_IMAGE_URL}/ic-chatbot.svg`,
       alt: 'chatbot icon',
     },
     sad: {
-      url: '/assets/ic-chatbot-sad.svg',
+      url: `${CLOUD_IMAGE_URL}/ic-chatbot-sad.svg`,
       alt: 'chatbot icon',
     },
   },
 
   chat: {
-    url: '/assets/ic-chat.svg',
+    url: `${CLOUD_IMAGE_URL}/ic-chat.svg`,
     alt: 'speech bubble icon',
   },
 
   send: {
-    url: '/assets/ic-send.svg',
+    url: `${CLOUD_IMAGE_URL}/ic-send.svg`,
     alt: 'send message icon',
   },
 
   trashcan: {
     normal: {
-      url: '/assets/ic-trashcan-normal.svg',
+      url: `${CLOUD_IMAGE_URL}/ic-trashcan-normal.svg`,
       alt: 'default trashcan icon',
     },
     hover: {
-      url: '/assets/ic-trashcan-hover.svg',
+      url: `${CLOUD_IMAGE_URL}/ic-trashcan-hover.svg`,
       alt: 'color trashcan icon',
     },
   },
 
   upload: {
     default: {
-      url: '/assets/ic-upload-default.svg',
+      url: `${CLOUD_IMAGE_URL}/ic-upload-default.svg`,
       alt: 'image upload icon',
     },
     active: {
-      url: '/assets/ic-upload-active.svg',
+      url: `${CLOUD_IMAGE_URL}/ic-upload-active.svg`,
       alt: 'activated image upload icon',
     },
     file: {
-      url: '/assets/ic-upload-file.svg',
+      url: `${CLOUD_IMAGE_URL}/ic-upload-file.svg`,
       alt: 'file upload icon',
     },
   },
 
   close: {
-    active: { url: '/assets/ic-close-active.svg', alt: 'close icon' },
+    active: { url: `${CLOUD_IMAGE_URL}/ic-close-active.svg`, alt: 'close icon' },
     default: {
-      url: '/assets/ic-close-default.svg',
+      url: `${CLOUD_IMAGE_URL}/ic-close-default.svg`,
       alt: 'close icon',
     },
   },
@@ -211,40 +213,40 @@ export const SVGS = {
   pagination: {
     left: {
       single: {
-        url: '/assets/ic-singleArrow-left.svg',
+        url: `${CLOUD_IMAGE_URL}/ic-singleArrow-left.svg`,
         alt: 'default left arrow',
       },
       double: {
-        url: '/assets/ic-doubleArrow-left.svg',
+        url: `${CLOUD_IMAGE_URL}/ic-doubleArrow-left.svg`,
         alt: 'default left double arrow ',
       },
     },
     right: {
       single: {
-        url: '/assets/ic-singleArrow-right.svg',
+        url: `${CLOUD_IMAGE_URL}/ic-singleArrow-right.svg`,
         alt: 'default right arrow',
       },
       double: {
-        url: '/assets/ic-doubleArrow-right.svg',
+        url: `${CLOUD_IMAGE_URL}/ic-doubleArrow-right.svg`,
         alt: 'default right double arrow ',
       },
     },
   },
 
   empty: {
-    url: '/assets/ic-empty-paper.svg',
+    url: `${CLOUD_IMAGE_URL}/ic-empty-paper.svg`,
     alt: 'no data icon',
   },
 
   map: {
     active: {
-      url: '/assets/ic-map-active.svg',
+      url: `${CLOUD_IMAGE_URL}/ic-map-active.svg`,
       alt: 'activated map icon',
     },
   },
 
   discord: {
-    url: '/assets/ic-discord.svg',
+    url: `${CLOUD_IMAGE_URL}/ic-discord.svg`,
     alt: 'discord icon',
   },
 };
